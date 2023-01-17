72b5db0f73d199ecb1d991b9d72325874436fc60
Wai Yan Htet <74424444+wa1yan@users.noreply.github.com>
2023-01-17 08:43:51 +0630

    add delete method

--- src/main/java/com/waiyanhtet/spring/data/jpa/repo/StateRepo.java
+++ src/main/java/com/waiyanhtet/spring/data/jpa/repo/StateRepo.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.waiyanhtet.spring.data.jpa.entity.State;
 
 public interface StateRepo extends JpaRepository<State, Integer>{
+	
+	State deleteByName(String name);
 
 	State findByName(String name);
+	
+	State deleteByRegion(String region);
 	
 	List<State> findByRegion(String region);
 	
